2b4d1f2433bf28665eb53f078c781dbb1811ac1b
Anastasie Vorobiova <vorobiovanastasie@gmail.com>
2022-04-03 23:22:57 +0200

    first commit

--- src/com/vorobiovanastasie/Main.java
+++ src/com/vorobiovanastasie/Main.java
@@ -3,6 +3,6 @@ package com.vorobiovanastasie;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello, qa.guru!");
     }
 }
